3deafa35975fad085a9f0601e7c9e24a1a71bb5d
Amber Conville <crebma@gmail.com>
2011-01-05 22:16:48 -0500

    amber: the beginning!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraftsmanGuild::Application.config.secret_token = 'd0f5e310fb9f4ca6fdc7a1f5dea6552a33897f24fc6221c7e330f3c04b95b5f9978cbe0cced4596b077e2cb93abc6970f86a777eb98dce1e7b6c1a87d7890dd8'
