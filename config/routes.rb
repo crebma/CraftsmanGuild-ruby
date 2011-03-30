fcf3fdd49a5b737317067d84621bd1e5dea6ffe0
Aaron Worsham <aaronworsham@gmail.com>
2011-03-29 20:16:07 -0400

    stuff and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ CraftsmanGuild::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :pages
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,7 +48,7 @@ CraftsmanGuild::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#index"
 
   # See how all your routes lay out with "rake routes"
 
